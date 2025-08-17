a67267aa52e54b68887795cff67cc7b2b1937d4a
MohamedDiaa <mohammeddiaato@gmail.com>
2025-08-17 21:53:46 +0300

    EXTI Config and Program Saving

--- MCAL/EXTI/EXTI_Config.h
+++ MCAL/EXTI/EXTI_Config.h
@@ -2,16 +2,49 @@
  * @file     EXTI_Config.h
  * @author   <Developer: Mohamed Diaa>
  * @author   <Reviewer>
- * @brief 
+ * @brief
  * @version   0.1
  * @date      2025-08-15
- * 
- * @copyright Copyright (c) 2025 , Gestell Company 
+ *
+ * @copyright Copyright (c) 2025 , Gestell Company
  */
-
 
 #ifndef _EXTI_CONFIG_H_
 #define _EXTI_CONFIG_H_
 
 
-#endif  
+
+/**
+ * @def SenseControl_Low
+ * @def SenseControl_AnyChange
+ * @def SenseControl_Falling
+ * @def SenseControl_Rising
+ * @brief Configuring SenseControl Logic Conditions for Each ISR
+ */
+
+#define SenseControl_Low        0
+#define SenseControl_AnyChange  1
+#define SenseControl_Falling    2
+#define SenseControl_Rising     3
+/**
+ * @def SenseControl_INT0
+ * @def SenseControl_INT1
+ * @def SenseControl_INT2
+ * @brief Configuring SenseControl value for Each ISR
+ */
+
+#define SenseControl_INT0 SenseControl_Low
+#define SenseControl_INT1 SenseControl_Low
+#define SenseControl_INT2 SenseControl_Low
+
+/**
+ * @def INT0_Enable
+ * @def INT1_Enable
+ * @def INT2_Enable
+ * @brief Configuring what pins to enable of Interrupt peripherals I can Use
+ */
+#define INT0_Enable Enable
+#define INT1_Enable Enable 
+#define INT2_Enable Disable
+
+#endif
--- MCAL/EXTI/EXTI_Program.c
+++ MCAL/EXTI/EXTI_Program.c
@@ -8,3 +8,75 @@
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
+#include "EXTI_Interface.h"
+#include "../RegistersAddress.h"
+#include "../../Common/Macro.h"
+#include "EXTI_Private.h"
+#include "EXTI_Config.h"
+#include <stdint.h>
+
+/**
+ * 
+ */
+
+void mEXTI0_Enable()
+{
+    /**
+     * SIE
+     * PIE Done in the program
+     * GICR
+     */
+
+} 
+
+void mEXTI1_Enable()
+{
+
+} 
+
+void mEXTI2_Enable()
+{
+
+} 
+
+void mEXTI_EnableALLWanted() 
+{
+    #if INT0_Enable==1
+        mEXTI0_Enable();
+    #endif
+    #if INT1_Enable==1
+        mEXTI1_Enable();
+    #endif
+    #if INT2_Enable==1
+        mEXTI2_Enable();
+    #endif
+
+}
+void __vector_1(void) 
+{
+
+}
+void __vector_2(void) 
+{
+
+}
+void __vector_3(void) 
+{
+
+}
+void mEXTI_DisableALL()
+{
+
+}
+void mEXTI0_Disable()
+{
+
+} 
+void mEXTI1_Disable()
+{
+
+} 
+void mEXTI2_Disable()
+{
+
+} 
