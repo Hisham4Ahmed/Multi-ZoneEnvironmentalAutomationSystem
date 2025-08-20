851e516573f8f8d63a5ed84a0b2273a0ccf9e3ea
حذيفة صديق <hoz009>
2025-08-20 13:50:55 +0300

    DIO_Driver ia added

--- MCAL/DIO/DIO_Interface.h
+++ MCAL/DIO/DIO_Interface.h
@@ -1,16 +1,54 @@
 /**
  * @file     DIO_Interface.h
- * @author   <Developer>
+ * @author   <Hozifa Ahmed>
  * @author   <Reviewer>
  * @brief 
- * @version   0.1
- * @date      2025-08-15
+ * @version   0.2
+ * @date      2025-08-20
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
 
 #ifndef _DIO_INTERFACE_H_
 #define _DIO_INTERFACE_H_
+#include <stdint.h>
+
+#define GroupA   1
+#define GroupB   2
+#define GroupC   3
+#define GroupD   4
+
+#define Pin0       0
+#define Pin1       1
+#define Pin2       2
+#define Pin3       3
+#define Pin4       4
+#define Pin5       5
+#define Pin6       6
+#define Pin7       7
+
+
+#define DIO_Input  0
+#define DIO_Output 1
+
+#define DIO_Low    0
+#define DIO_High   1
+
+
+void DIO_Direction_Pin(uint8_t GroupName , uint8_t PinNumber , uint8_t DirectionState);
+void DIO_Write_Pin(uint8_t GroupName ,  uint8_t PinNumber , uint8_t OutputType);
+uint8_t DIO_Read_Pin(uint8_t  GroupName, uint8_t PinNumber);
+
+
+
+void DIO_Direction_Group(uint8_t GroupName , uint8_t DirecationValue);
+void DIO_Write_Group(uint8_t GroupName ,  uint8_t OutputValue);
+uint8_t DIO_Read_Group(uint8_t  GroupName);
+
+
+
+
+
 
 
 #endif /*_DIO_INTERFACE_H_*/
--- MCAL/DIO/DIO_Private.h
+++ MCAL/DIO/DIO_Private.h
@@ -1,16 +1,52 @@
 /**
  * @file     DIO_Private.h
- * @author   <Developer>
+ * @author   <Hozifa Ahmed>
  * @author   <Reviewer>
  * @brief 
- * @version   0.1
- * @date      2025-08-15
+ * @version   0.2
+ * @date      2025-08-17
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
 
 #ifndef _DIO_PRIVATE_H_
 #define _DIO_PRIVATE_H_
+#include <stdint.h>
+
+
+//Direction Pins
+#define DDRA_REG  *((volatile uint8_t*)0x3A)
+#define DDRB_REG  *((volatile uint8_t*)0x37)
+#define DDRC_REG  *((volatile uint8_t*)0x34)
+#define DDRD_REG  *((volatile uint8_t*)0x31)
+
+
+
+
+//Input Pins
+#define PORTA_REG *((volatile uint8_t*)0x3B)
+#define PORTB_REG *((volatile uint8_t*)0x38)
+#define PORTC_REG *((volatile uint8_t*)0x35)
+#define PORTD_REG *((volatile uint8_t*)0x32)
+
+
+
+
+//Output pins
+#define PINA_REG  *((volatile uint8_t*)0x39)
+#define PINB_REG  *((volatile uint8_t*)0x36)
+#define PINC_REG  *((volatile uint8_t*)0x33)
+#define PIND_REG  *((volatile uint8_t*)0x30)
+
+
+
+
+
+
+
+
+
 
 
 #endif /*_DIO_PRIVATE_H_*/
+
