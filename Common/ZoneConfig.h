737e61b9d30b4022f74461613842a689774cf3a5
حذيفة صديق <hoz009>
2025-08-27 23:04:25 +0300

    LED_for_zoncofig

--- Common/ZoneConfig.h
+++ Common/ZoneConfig.h
@@ -1,11 +1,10 @@
 /**
- * @file     ZoneConfig.h
- * @author   <Developer>
- * @author   <Reviewer>
- * @brief 
- * @version   0.1
- * @date      2025-08-15
- * 
+ * @file      ZoneConfig.h
+ * @author    Developer : Hozifa Ahmed
+ * @author    Reviewer :  Ahmed Ashraf
+ * @brief     Zone Hardware Mapping Configuration (LED pins) 
+ * @version   0.2
+ * @date      2025-08-27
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
 
@@ -13,7 +12,25 @@
 #ifndef _ZONECONFIG_H_
 #define _ZONECONFIG_H_
 
+/* ------------------- Zone 1 ------------------- */
+/** @brief Zone 1 LED connection */
+#define ZONE1_LED_PORT   GroupC
+#define ZONE1_LED_PIN    0
 
+/* ------------------- Zone 2 ------------------- */
+/** @brief Zone 2 LED connection */
+#define ZONE2_LED_PORT   GroupC
+#define ZONE2_LED_PIN    2
+
+/* ------------------- Zone 3 ------------------- */
+/** @brief Zone 3 LED connection */
+#define ZONE3_LED_PORT   GroupC
+#define ZONE3_LED_PIN    4
+
+/* ------------------- Zone 4 ------------------- */
+/** @brief Zone 4 LED connection */
+#define ZONE4_LED_PORT   GroupC
+#define ZONE4_LED_PIN    6
 
 
 #endif /* _ZONECONFIG_H_ */
