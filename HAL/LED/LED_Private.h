0214ce94dd93820fbfc2b7f39ccd41da728697c6
حذيفة صديق <hoz009>
2025-08-28 15:24:19 +0300

    Last_UPdate_After_review_adding_ARR_For_Led_Driver

--- HAL/LED/LED_Private.h
+++ HAL/LED/LED_Private.h
@@ -3,8 +3,8 @@
  * @author    Developer : Hozifa Ahmed
  * @author    Reviewer :  Ahmed Ashraf
  * @brief     Private macros and helpers for LED Driver
- * @version   0.2
- * @date      2025-08-27
+ * @version   0.3
+ * @date      2025-08-28
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
 
@@ -17,16 +17,21 @@
  *  @def LED_ACTIVE_HIGH
  *  @brief LED connected as source (ON = High, OFF = Low)
  */
-
 #define LED_ACTIVE_HIGH   1
 
 /**
  *  @def LED_ACTIVE_LOW
  *  @brief LED connected as sink (ON = Low, OFF = High)
  */
-
 #define LED_ACTIVE_LOW    0
 
+
+/* --------- Struct for LED Mapping --------- */
+typedef struct 
+{
+    uint8_t Port;   /**< LED Port (GroupA..GroupD) */
+    uint8_t Pin;    /**< LED Pin  (0..7) */
+} LED_ConfigType;
 
 
 
