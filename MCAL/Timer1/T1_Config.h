eb3acbad5469b76683b1b72c0e4821e82a196a05
Hesham Ahmed <Hisham4Ahmed@gmail.com>
2025-08-30 06:18:10 +0300

    - Add the Files Arch for the Timer1 and Timer2 Task

---
+++ MCAL/Timer1/T1_Config.h
@@ -0,0 +1,21 @@
+/**
+ * @file T1_Config.h
+ * @author Hesham Ahmed  (Hisham4Ahmed@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-30
+ * 
+ * @copyright Copyright (c) 2025 , Gestell Company 
+ * 
+ */
+
+#ifndef _T1_CONFIG_H_
+#define _T1_CONFIG_H_
+
+
+
+
+
+
+
+#endif /*_T1_CONFIG_H_*/
---
+++ MCAL/Timer1/T1_Interface.h
@@ -0,0 +1,18 @@
+/**
+ * @file T1_Interface.h
+ * @author Hesham Ahmed  (Hisham4Ahmed@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-30
+ * 
+ * @copyright Copyright (c) 2025 , Gestell Company 
+ * 
+ */
+
+
+#ifndef _T1_INTERFACE_H_
+#define _T1_INTERFACE_H_
+
+
+
+#endif /*_T1_INTERFACE_H_*/
