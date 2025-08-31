14e8f8c907ea6c044fb6d8b27997bf416e3ecddd
Mohammed Atif <medoatifatif@gmail.com>
2025-08-31 21:34:48 +0300

    Add Functions Prototypes & pseudo code

--- HAL/LM35/LM35_Interface.h
+++ HAL/LM35/LM35_Interface.h
@@ -1,10 +1,10 @@
 /**
  * @file     LM35_Interface.h
- * @author   <Developer>
- * @author   <Reviewer>
+ * @author   Developer : Mohammed Atif
+ * @author   Reviewer  : Mohammed Diaa
  * @brief 
  * @version   0.1
- * @date      2025-08-15
+ * @date      2025-08-31
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
@@ -12,6 +12,9 @@
 #ifndef _LM35_INTERFACE_H_
 #define _LM35_INTERFACE_H_
 
+#include <stdint.h>
 
+void hLm35_Init(uint8_t ZoneNumber);
+int8_t hLm35_GetTemp(uint8_t ZoneNumber);
 
 #endif /*_LM35_INTERFACE_H_*/
--- HAL/LM35/LM35_Program.c
+++ HAL/LM35/LM35_Program.c
@@ -1,10 +1,10 @@
 /**
  * @file     LM35_Program.c
- * @author   <Developer>
- * @author   <Reviewer>
+ * @author   Developer : Mohammed Atif
+ * @author   Reviewer  : Mohammed Diaa
  * @brief 
  * @version   0.1
- * @date      2025-08-15
+ * @date      2025-08-31
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
@@ -14,5 +14,22 @@
 
 #include "LM35_Interface.h"
 
+void hLm35_Init(uint8_t ZoneNumber) {
+    // Initialize ADC :
+        // no need for Zone Number !
+}
+
+int8_t hLm35_GetTemp(uint8_t ZoneNumber) {
+    // Start single mode conversion for :
+        // Zone Number
+        // Takes Channel -> Zoneconfig.h 0-7
+    
+    // Convert Digital to Analog volt in mV.
+
+    // Vin = 10mV * T(C)
+    // T(C) = Vin / 10mV
+    
+    // return T(C)
+}
 
 #endif /*LM35_Driver*/
