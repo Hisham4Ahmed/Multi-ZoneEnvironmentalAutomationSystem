653ffced388ce7ec2762086afe323482b6e01257
MohamedDiaa <mohammeddiaato@gmail.com>
2025-09-03 00:32:04 +0300

    Finishing the PWM INIT Function

--- MCAL/Timer2/T2_Program.c
+++ MCAL/Timer2/T2_Program.c
@@ -14,9 +14,40 @@
 #if TIMER2_Driver
 
 #include "T2_Interface.h"
+#include "../../Common/Macro.h"
 #include "../RegistersAddress.h"
 
 
+void mTimer2_InitFastPWMMode()
+{
+	uint8_t TCCR2_Temp  = 0 ;
+    //fast mode  1  1
+      SetBit(TCCR2_Temp,WGM20_Bit);
+      SetBit(TCCR2_Temp,WGM21_Bit);
+      #if CompareOutputMode ==NonInverting
+            ClearBit(TCCR2_Temp,COM20_Bit);
+            SetBit(TCCR2_Temp,COM21_Bit);
+            
+    #elif CompareOutputMode ==Inverting
+            SetBit(TCCR2_Temp,COM20_Bit);
+            SetBit(TCCR2_Temp,COM21_Bit);
+
+    #endif
+
+    mTimer2_ChangeDutyCycle(initialDutyCycle);
+
+    TCCR2_Temp &=ClearPrescallerbitsOnlyMask;
+    TCCR2_Temp |=Timer2_Prescaller;
+
+    TCCR2_Reg=TCCR2_Temp;
+
+ 
+}
+
+void mTimer2_ChangeDutyCycle(uint8_t DutyCycle );
+
+
+void mTimer2_FastPWMStop(void);
 
 
 
