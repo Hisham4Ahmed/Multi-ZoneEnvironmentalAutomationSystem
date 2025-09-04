b56a1968aa35161cd1a8329ee5e847e6fc47b54f
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-09-04 15:09:43 +0300

    restoring back

--- HAL/LM35/LM35_Config.h
+++ HAL/LM35/LM35_Config.h
@@ -12,6 +12,4 @@
 #ifndef _LM35_CONFIG_H_
 #define _LM35_CONFIG_H_
 
-#define LM35_Mode   Basic
-
 #endif /*_LM35_CONFIG_H_*/
--- HAL/LM35/LM35_Interface.h
+++ HAL/LM35/LM35_Interface.h
@@ -12,41 +12,4 @@
 #ifndef _LM35_INTERFACE_H_
 #define _LM35_INTERFACE_H_
 
-#include <stdint.h>
-
-#include "../../Common/ZoneConfig.h"
-#include "LM35_Config.h"
-#include "LM35_Private.h"
-#include "../../MCAL/ADC/ADC_Interface.h"
-#include "../../MCAL/DIO/DIO_Interface.h"
-
-/**
- * @fn void hLm35_Init(uint8_t ZoneNumber)
- * @param ZoneNumber Zone number of the LM35 sensor.
- * 
- * @brief Initializes the LM35 temperature sensor for a given zone.
- * 
- * @details
- * Configures the pin connected to the LM35 sensor as input
- * and initializes the ADC module for analog-to-digital conversion.
- *
- */
-void hLm35_Init(uint8_t ZoneNumber);
-
-/**
- * @fn int8_t hLm35_GetTemp(uint8_t ZoneNumber)
- * @param ZoneNumber The LM35 zone index (1-based).
- * 
- * @brief Gets the temperature in Celsius from the LM35 sensor.
- * 
- * @details
- * Reads the ADC value from the specified zone, converts it to
- * millivolts, and then converts that to temperature in Celsius
- * (10 mV per °C). If the ADC reading hasn't changed since the
- * last call, the cached temperature value is returned.
- *
- * @return Temperature in Celsius (int8_t).
- */
-int8_t hLm35_GetTemp(uint8_t ZoneNumber);
-
 #endif /*_LM35_INTERFACE_H_*/
