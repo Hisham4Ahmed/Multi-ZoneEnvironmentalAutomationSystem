5f6b1e894128b755fda75f9bc7405be36bfb2131
Mohammed Atif <medoatifatif@gmail.com>
2025-09-08 15:10:33 +0300

    Full implementation of the Driver

--- APP/ModeControl/ModeControl_Program.c
+++ APP/ModeControl/ModeControl_Program.c
@@ -1,10 +1,33 @@
 /**
  * @file     ModeControl_Program.c
- * @author   <Developer>
+ * @author   Developer: Mohammed Atif   "medoatifatif@gmail.com"
  * @author   <Reviewer>
- * @brief 
+ * @brief    Mode control initialization and mode retrieval.
  * @version   0.1
- * @date      2025-08-15
+ * @date      2025-09-8
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
+#include "ModeControl_Interface.h"
+
+/**
+ * @var CurrentMode
+ * @brief Current operation mode of the system.
+ *
+ * Possible values:
+ * - 0: Automatic mode
+ * - 1: Manual mode
+ */
+extern uint8_t CurrentMode; 
+
+void ModeControl_Init(void) {
+    hBtn_Init();
+}
+
+void ModeControl_Task(void) {
+    // Empty for current version
+}
+
+uint8_t ModeControl_GetMode(void) {
+    return CurrentMode;
+}
