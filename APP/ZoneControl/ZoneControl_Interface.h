55f248c5f7948dc1b85a6fc1e7a09b73d8ed4168
Mohammed Atif <medoatifatif@gmail.com>
2025-09-14 21:11:28 +0300

    Adding prototypes and include required header files

--- APP/ZoneControl/ZoneControl_Interface.h
+++ APP/ZoneControl/ZoneControl_Interface.h
@@ -1,7 +1,7 @@
 /**
  * @file     ZoneControl_Interface.h
- * @author   <Developer>
- * @author   <Reviewer>
+ * @author   Developer: Mohammed Atif   "medoatifatif@gmail.com"
+ * @author   Reviewer:  Hesham Ahmed    (Hisham4Ahmed@gmail.com)
  * @brief 
  * @version   0.1
  * @date      2025-08-15
@@ -12,7 +12,12 @@
 #ifndef _ZONECONTROL_INTERFACE_H_
 #define _ZONECONTROL_INTERFACE_H_
 
+#include "../ModeControl/ModeControl_Interface.h"
+#include "../../Common/ZoneConfig.h"
+#include "../../HAL/LDR/LDR_Interface.h"
+#include "../../HAL/LM35/LM35_Interface.h"
 
-
+void ZoneControl_Init(void);
+void ZoneControl_Task(void);
 
 #endif /* _ZONECONTROL_INTERFACE_H_ */
--- APP/ZoneControl/ZoneControl_Program.c
+++ APP/ZoneControl/ZoneControl_Program.c
@@ -1,10 +1,22 @@
 /**
  * @file     ZoneControl_Program.c
- * @author   <Developer>
- * @author   <Reviewer>
+ * @author   Developer: Mohammed Atif   "medoatifatif@gmail.com"
+ * @author   Reviewer:  Hesham Ahmed    (Hisham4Ahmed@gmail.com)
  * @brief 
  * @version   0.1
  * @date      2025-08-15
  * 
  * @copyright Copyright (c) 2025 , Gestell Company 
  */
+#include "../../Common/Config.h"
+#if ZoneControl_App
+ 
+void ZoneControl_Init(void) {
+
+}
+
+void ZoneControl_Task(void) {
+
+}
+
+#endif
