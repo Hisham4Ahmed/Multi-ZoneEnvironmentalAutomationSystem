7a3d73067af5df6e6b7c0823478de3fba92c1023
Ahmed Ashraf <ahmedashraf2022222@gmail.com>
2025-09-14 07:52:12 +0300

    Communication App Files Was Created

---
+++ APP/Communication/Communication_Private.h
@@ -0,0 +1,9 @@
+/**
+ * @file     Communication_Program.h
+ * @author   Developer: Naira Mohammed (naira.mohammed.76@gmail.com)
+ * @author   Developer: Ahmed Ashraf    (ahmedashraf2022222@gmail.com)
+ * @brief    Contains private definitions and configurations for the Communication application.
+ * @version   0.1
+ * @date      2025-09-14
+ * @copyright Copyright (c) 2025 , Gestell Company 
+ */
